5ba607772141f995f9c8a2c0f7b5d6b0ada8b6cd
Altureny <altureny17@gmail.com>
2026-09-13 04:08:55 +0300

    Fix import case for signup_screen.dart

--- rideon-driver-app-production_1.2/lib/presentation/screens/auth/login_screen.dart
+++ rideon-driver-app-production_1.2/lib/presentation/screens/auth/login_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 import 'package:ride_on_driver/core/utils/translate.dart';
-import 'package:ride_on_driver/presentation/screens/Auth/signup_screen.dart';
+import 'package:ride_on_driver/presentation/screens/auth/signup_screen.dart';
 import 'package:ride_on_driver/presentation/screens/onboarding/language_select_screen.dart';
 import '../../../core/extensions/workspace.dart';
 import '../../../core/utils/common_widget.dart';
