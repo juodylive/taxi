20228dbe4fb7e297cfbea06a2bfb45386d275046
Altureny <altureny17@gmail.com>
2026-09-13 04:20:20 +0300

    Fix import path for payout_screen.dart

--- rideon-driver-app-production_1.2/lib/presentation/screens/payment/driver_wallet.dart
+++ rideon-driver-app-production_1.2/lib/presentation/screens/payment/driver_wallet.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:ride_on_driver/core/utils/translate.dart';
-import 'package:ride_on_driver/presentation/screens/Payment/payout_screen.dart';
+import 'package:ride_on_driver/presentation/screens/payment/payout_screen.dart';
 import 'package:ride_on_driver/presentation/screens/payment/wallet_recharge_screen.dart';
 
 import '../../../core/extensions/workspace.dart';
