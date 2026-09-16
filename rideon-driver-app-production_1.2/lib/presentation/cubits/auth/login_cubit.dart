d523b2109a1cd34a254cda3a33fe90effe9b2183
Altureny <altureny17@gmail.com>
2026-09-16 06:03:10 +0300

    Update auth_repository import path in login_cubit.dart

--- rideon-driver-app-production_1.2/lib/presentation/cubits/auth/login_cubit.dart
+++ rideon-driver-app-production_1.2/lib/presentation/cubits/auth/login_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:ride_on_driver/data/repositories/auth_repository.dart';
+import 'package:zearah_driver/data/repositories/auth_repository.dart';
 
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
