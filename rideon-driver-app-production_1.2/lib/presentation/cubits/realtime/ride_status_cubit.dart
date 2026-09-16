47c32035e82ec4b13f1276aa77970c6bef51c606
Altureny <altureny17@gmail.com>
2026-09-16 05:08:51 +0300

    Update import path for realtime_repository

--- rideon-driver-app-production_1.2/lib/presentation/cubits/realtime/ride_status_cubit.dart
+++ rideon-driver-app-production_1.2/lib/presentation/cubits/realtime/ride_status_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:ride_on_driver/data/repositories/realtime_repository.dart';
+import 'package:zearah_driver/data/repositories/realtime_repository.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
