e883d03d15c98ccfb0fe910df665b03531d3c837
Altureny <altureny17@gmail.com>
2026-09-20 02:17:11 +0300

    Add flutter_map and latlong2 imports to ride_screen.dart

--- rideon-driver-app-production_1.2/lib/presentation/screens/search/ride_screen.dart
+++ rideon-driver-app-production_1.2/lib/presentation/screens/search/ride_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:zearah_driver/core/utils/translate.dart';
 import '../../../core/extensions/workspace.dart';
